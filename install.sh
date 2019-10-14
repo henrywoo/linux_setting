bcf6482cfa581d67f9dc4b2b0db537c8ce9fa869
HenryAtUber <henry.wu@uber.com>
2019-10-13 19:32:48 -0700

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v netstat)" ]; then
+  apt-get install net-tools -y
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+  apt-get install git -y
+fi
+
+if ! [ -x "$(command -v python)" ]; then
+  apt-get install python -y
+fi
+
+if ! [ -x "$(command -v vim)" ]; then
+  apt-get install python -y
+fi
+
+if ! [ -x "$(command -v ctags)" ]; then
+  apt-get install ctags -y
+fi
+
 pushd /tmp
 git clone https://github.com/henrywoo/linux_setting.git
 cd linux_setting
